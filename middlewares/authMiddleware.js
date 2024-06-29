60652944d558772720cd83cbe54a32573f24ee88
Dipak Pandey <dipak.pandey@aticanlabs.com>
2024-06-29 16:09:17 +0530

    auth-jwt

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
