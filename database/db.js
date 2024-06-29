2677ec217453d41bac283d283623934a8daaf86b
Dipak Pandey <dipak.pandey@aticanlabs.com>
2024-06-29 16:08:32 +0530

    config db

---
+++ database/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+    if (!mongoURI) {
+      throw new Error('MongoDB URI is not defined in the environment variables');
+    }
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
